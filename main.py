2ac69e205b1290b3252af25536e403a442f34c84
DadooCoding <dadoo.coding@gmail.com>
2021-06-04 15:39:48 -0500

    make database url an environmental var

--- main.py
+++ main.py
@@ -14,12 +14,12 @@ from functools import wraps
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = os.environ.get("FLASK_API")
-print(f"API IS {os.environ.get('FLASK_API')}")
+# print(f"API IS {os.environ.get('FLASK_API')}")
 ckeditor = CKEditor(app)
 Bootstrap(app)
-
+# 'sqlite:///blog.db'
 # CONNECT TO DB
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///blog.db'
+app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get("DATABASE_URL")
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
 
