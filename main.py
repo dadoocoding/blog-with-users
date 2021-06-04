e40a52cc4a1ffb5b7d95f13e526f94d039978eea
DadooCoding <dadoo.coding@gmail.com>
2021-06-04 15:43:02 -0500

    make database url an environmental var

--- main.py
+++ main.py
@@ -19,7 +19,7 @@ ckeditor = CKEditor(app)
 Bootstrap(app)
 # 'sqlite:///blog.db'
 # CONNECT TO DB
-app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get("DATABASE_URL")
+app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get("DATABASE_URL",  "sqlite:///blog.db")
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
 
